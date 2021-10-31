c12f5a6693663a059c96b18561733e11ab32689a
rohith <nakkarohithkumar@gmail.com>
2021-10-31 07:41:32 +0530

    deploy

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.get("/signup", (req, res) => {
 
 if (process.env.NODE_ENV === "Production") {
   app.use(express.static("client/build"));
+  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
